00deff9b5c9f668b0ddffb8d442670aabef36d4b
trek-ash <yashsinghgreat@gmail.com>
2020-03-25 12:55:39 +0530

    port update to env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 8000;
+const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const movies = require('./routes/api/movies')
