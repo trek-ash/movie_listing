cf76308e8f472bc286f238119aab4585e89953f4
trek-ash <yashsinghgreat@gmail.com>
2020-03-25 20:43:29 +0530

    base url fix for client api

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,3 @@
 export const api_key = '9500fbf690cb228f96fef08854b76a46';
 export const movie_api_base_url = 'https://api.themoviedb.org/3';
-export const api_base_url = 'http://localhost:8000';
+export const api_base_url = 'https://movie-finder-20.herokuapp.com';
