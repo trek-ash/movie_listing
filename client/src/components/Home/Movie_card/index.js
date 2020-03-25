d427c12bd3069bfa0c15ed161aae67c8c7cd0359
trek-ash <yashsinghgreat@gmail.com>
2020-03-25 15:08:18 +0530

    updated atlas conf

--- client/src/components/Home/Movie_card/index.js
+++ client/src/components/Home/Movie_card/index.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './card.css'
+const add_icon = require('../../../assets/icons/heart.png');
+const fav_icon = require('../../../assets/icons/added_heart.png');
+const remove_icon = require('../../../assets/icons/remove.png')
 const MovieCard = (props) =>{
+    const [isFav, setFav] = useState(props.isFavourite);
     return (
         <div className="card mt-4">
             <div className="card-header">
                 {props.movie.original_title || props.movie.title}
                 {   
-                    props.isFavourite?
+                    isFav?
                     <>
                     <span 
-                    className="float-right"
-                    onClick={()=>props.removeFromFavourite(props.movie.movie_id)}>
-                        Remove from favourite
+                    className="float-right ml-3"
+                    onClick={()=>{props.removeFromFavourite(props.movie.movie_id || props.movie.id); setFav(false)}}>
+                        {/* Remove from favourite */}
+                        
+                        <img className="del-image cursor-pointer" src={remove_icon} alt="Remove"/>
                     </span>
                     </>
                     :<span 
                     className="float-right"
-                    onClick={()=>props.addToFavourite(props.movie.original_title, props.movie.overview, props.movie.id )}>
-                        Add to favourite
+                    onClick={()=>{props.addToFavourite(props.movie.original_title, props.movie.overview, props.movie.id );setFav(true);}}>
+                    <   img className="del-image cursor-pointer" src={add_icon} alt="Add"/>
+                        
                     </span>    
                 }
                 
-                {props.movie.original_title}
+                
             </div>
             <div className="card-body">{props.movie.overview}</div>
             <div className="card-footer">
-                <span className="float-right"><b>Rating:</b> {props.movie.vote_average}</span>
+            
+                {isFav?<img className="fav-image" src={fav_icon} alt="Fav"/>: ''}
+                {props.movie.vote_average?<span className="float-right"><b>Rating:</b> {props.movie.vote_average}</span>: ''}
             </div>
         </div>
     )
